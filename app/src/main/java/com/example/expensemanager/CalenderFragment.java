bcb1a91765dbeb6a56e743714dda37e91b4b0135
Swaroop B A <swaroop.ba@videojet.com>
2021-03-12 03:17:59 +0530

    Next and Prev Buttons Working

--- app/src/main/java/com/example/expensemanager/CalenderFragment.java
+++ app/src/main/java/com/example/expensemanager/CalenderFragment.java
@@ -14,23 +14,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalenderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
 public class CalenderFragment extends Fragment {
+
+    private final String kDateString = "DateString";
+    private final String kIsItCurrentDate = "IsItCurrentDate";
 
     public interface OnMessageTransaction
     {
-        public void onMessageSend(String message);
+        public void onMonthYearPressed(String message);
+        public void onPreviousMonthPressed(String message);
+        public void onNextMonthPressed(String message);
+        public void onDatePressed(String message);
     }
 
-
-    // TODO: Rename and change types of parameters
-    private String currentDateString;
-    private Button last;
+    private String dateString;
+    private Boolean isItCurrentDate;
     private OnMessageTransaction onMessageTransaction;
+    private Integer date;
+    private Integer month;
+    private Integer year;
+    private Integer numDays;
 
     public CalenderFragment() {
         // Required empty public constructor
@@ -54,7 +62,8 @@ public class CalenderFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            currentDateString = getArguments().getString("Hi");
+            dateString = getArguments().getString(kDateString);
+            isItCurrentDate = getArguments().getBoolean(kIsItCurrentDate);
         }
     }
 
@@ -63,14 +72,70 @@ public class CalenderFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_calender, container, false);
-        last = (Button) view.findViewById(R.id.last);
-        Button date = (Button) view.findViewById(R.id.date);
-        date.setOnClickListener(new View.OnClickListener() {
+
+        String dateList[] = dateString.split("/");
+        date = Integer.parseInt(dateList[0]);
+        month = Integer.parseInt(dateList[1]);
+        year = Integer.parseInt(dateList[2]);
+
+        Calendar c2 = Calendar.getInstance();
+        c2.set(year, month-1, date);
+        Log.d("ERROR",c2.toString());
+        c2.set(Calendar.DAY_OF_MONTH, 1);
+        Integer startDateMonth = c2.get(Calendar.DAY_OF_WEEK);
+        Integer totalDaysInMonth = c2.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+
+        numDays = 1;
+        for(Integer i = 1;i <= 42; i++ )
+        {
+            String id = "date" + i;
+            Button refButton = view.findViewById(getResources().getIdentifier(id, "id", getContext().getPackageName()));
+            Integer actualId = Integer.parseInt((String) refButton.getTag());
+
+
+            if (actualId < startDateMonth || actualId > (totalDaysInMonth + startDateMonth - 1))
+            {
+                refButton.setVisibility(View.INVISIBLE);
+            }
+            else
+            {
+                refButton.setText(numDays.toString());
+                refButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sendDate(refButton.getText().toString());
+                    }
+                });
+                numDays = numDays + 1;
+            }
+        }
+
+        Button tempDate = (Button) view.findViewById(R.id.date);
+        tempDate.setText(getMonthName(month)+" "+year);
+        tempDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onMessageTransaction.onMessageSend("How are you");
+                onMessageTransaction.onMonthYearPressed("How are you");
             }
         });
+
+        Button lastButton = view.findViewById(R.id.last);
+        lastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onMessageTransaction.onPreviousMonthPressed(date+"/"+month+"/"+year);
+            }
+        });
+
+        Button nextButton = view.findViewById(R.id.next);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onMessageTransaction.onNextMonthPressed(date+"/"+month+"/"+year);
+            }
+        });
+
         return view;
     }
 
@@ -78,8 +143,33 @@ public class CalenderFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        last.setText(currentDateString);
+
     }
 
-    
+    private void sendDate(String day)
+    {
+        String date = day + "/" + month + "/" + year;
+        onMessageTransaction.onDatePressed(date);
+    }
+
+    private String getMonthName(Integer num)
+    {
+        String res = "";
+        switch (num)
+        {
+            case 1: res = "Jan"; break;
+            case 2: res = "Feb"; break;
+            case 3: res = "Mar"; break;
+            case 4: res = "Apr"; break;
+            case 5: res = "May"; break;
+            case 6: res = "June"; break;
+            case 7: res = "July"; break;
+            case 8: res = "Aug"; break;
+            case 9: res = "Sep"; break;
+            case 10: res = "Oct"; break;
+            case 11: res = "Nov"; break;
+            case 12: res = "Dec"; break;
+        }
+        return res;
+    }
 }
--- app/src/main/java/com/example/expensemanager/EntranceActivity.java
+++ app/src/main/java/com/example/expensemanager/EntranceActivity.java
@@ -5,34 +5,104 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class EntranceActivity extends AppCompatActivity implements CalenderFragment.OnMessageTransaction {
+
+    private final String kDateString = "DateString";
+    private final String kIsItCurrentDate = "IsItCurrentDate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addFragment();
+
+        Calendar cal = Calendar.getInstance();
+        String date = cal.get(Calendar.DAY_OF_MONTH)+"/"+(cal.get(Calendar.MONTH)+1)+"/"+cal.get(Calendar.YEAR);
+        addFragment(date, true, false);
     }
 
-    private void addFragment()
+    @Override
+    public void onPreviousMonthPressed(String message) {
+        Log.d("ERROR", message);
+        ArrayList<Integer> current = getDateFromString(message);
+
+        Integer currentMonth = current.get(1);
+        Integer prevMonth;
+        if (currentMonth == 1)
+        {
+               prevMonth = 12;
+               current.set(2, current.get(2)-1);
+        }
+        else {
+            prevMonth = currentMonth - 1;
+        }
+        addFragment(current.get(0)+"/"+prevMonth+"/"+current.get(2), false, true);
+    }
+
+    @Override
+    public void onNextMonthPressed(String message) {
+        ArrayList<Integer> current = getDateFromString(message);
+
+        Integer currentMonth = current.get(1);
+        Integer nextMonth;
+        if (currentMonth == 12)
+        {
+            nextMonth = 1;
+            current.set(2, current.get(2)+1);
+        }
+        else{
+            nextMonth = currentMonth + 1;
+        }
+
+        addFragment(current.get(0)+"/"+nextMonth+"/"+current.get(2), false, true);
+
+    }
+
+    @Override
+    public void onDatePressed(String message) {
+
+    }
+
+    private ArrayList<Integer> getDateFromString(String date)
+    {
+        String str[] = date.split("/");
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        for(String i: str)
+        {
+            ret.add(Integer.parseInt(i));
+        }
+        return  ret;
+    }
+
+    private void addFragment(String date, Boolean isCurrentDate, Boolean isReplaceFragment)
     {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
         Bundle bundle = new Bundle();
-        bundle.putString("Hi","Blade");
+        bundle.putString(kDateString, date);
+        bundle.putBoolean(kIsItCurrentDate, isCurrentDate);
+
         CalenderFragment calenderFragment = new CalenderFragment();
         calenderFragment.setArguments(bundle);
-        fragmentTransaction.add(R.id.fragmentContainer, calenderFragment);
+        if(!isReplaceFragment) {
+            fragmentTransaction.add(R.id.fragmentContainer, calenderFragment);
+        }else{
+            fragmentTransaction.replace(R.id.fragmentContainer, calenderFragment);
+        }
         fragmentTransaction.commit();
 
     }
 
     @Override
-    public void onMessageSend(String message) {
+    public void onMonthYearPressed(String message) {
 
         Toast.makeText(this,message,Toast.LENGTH_LONG).show();
     }
